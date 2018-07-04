e244ee11e7ff1c1b2fe3ebc8b426fd796ebbe497
Kailang Fu <kailangfu@gmail.com>
2018-07-04 13:57:57 -0700

    Enable CORS

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ mongoose.connect('mongodb://localhost:27017/nezphere', err => {
 });
 
 const app = require('express')();
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const routes = require('./routes');
 app.use('/v1', routes);
